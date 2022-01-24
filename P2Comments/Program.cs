00ab965a2e17c4ab1b088a79322f46303d76c1bd
Joakimblom <joablo90@outlook.com>
2022-01-24 17:31:57 +0100

    Finished P2 Comments

---
+++ P2Comments/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace SureCan
+
+/*
+ * Joakim Blomstrand
+ * 2022-01-24
+ * joakim.blomstrand@student.forsbergsskola.se
+ */
+{
+    /// <summary>
+    /// Main method acts as entry point for the program.
+    /// When the application is started, the Main method is the first method that is invoked.
+    /// </summary>
+    class Program1
+    {
+        private static void Main()
+        {
+            //This lets you write whatever you put between the ""
+            Console.WriteLine("Hello World");
+            
+        }
+    }  
+}
