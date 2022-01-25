abb519b780375115fb945f20aeffffdf830b806b
Joakimblom <joablo90@outlook.com>
2022-01-25 13:24:34 +0100

    Finished P4Data Types

--- P2Comments/Program.cs
+++ P2Comments/Program.cs
@@ -14,6 +14,7 @@
     {
         private static void Main()
         {
+            
             //This lets you write whatever you put between the ""
             Console.WriteLine("Hello World");
             
