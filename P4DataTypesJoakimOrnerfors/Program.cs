0d40892b2f87ca60b5bd2c8d98cc7c1877d79c12
JoakimOrnerfors <joakimornerfors@gmail.com>
2022-01-27 00:55:35 +0100

    Finsihed P4

---
+++ P4DataTypesJoakimOrnerfors/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Types;
+class Datatypes
+
+{
+private static void Main()
+    {
+    float decimalValue =(float)0.3;
+    string OutputString ="Hello World!";
+    bool isAwesome =true;
+    byte hexByte =0xA7;
+    Console.WriteLine(OutputString);Console.WriteLine(decimalValue); 
+    Console.WriteLine(isAwesome);Console.WriteLine(hexByte);
+    }
+}
+   
+  
+
