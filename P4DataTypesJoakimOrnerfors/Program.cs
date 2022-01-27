66a1ac0248780e5523bce98c6926531c62b71bf0
JoakimOrnerfors <joakimornerfors@gmail.com>
2022-01-27 01:03:00 +0100

    Fisnihed p4DataTypes

--- P4DataTypesJoakimOrnerfors/Program.cs
+++ P4DataTypesJoakimOrnerfors/Program.cs
@@ -1,17 +1,18 @@
 ﻿namespace Types;
+
 class Datatypes
 
 {
-private static void Main()
+    private static void Main()
     {
-    float decimalValue =(float)0.3;
-    string OutputString ="Hello World!";
-    bool isAwesome =true;
-    byte hexByte =0xA7;
-    Console.WriteLine(OutputString);Console.WriteLine(decimalValue); 
-    Console.WriteLine(isAwesome);Console.WriteLine(hexByte);
+        float decimalValue =(float)0.3;
+        string OutputString ="Hello World!";
+        bool isAwesome =true;
+        byte hexByte =0xA7;
+        Console.WriteLine(OutputString);Console.WriteLine(decimalValue); 
+        Console.WriteLine(isAwesome);Console.WriteLine(hexByte);
     }
 }
-   
-  
+
+
 
